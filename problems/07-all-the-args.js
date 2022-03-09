873e670b891181740531190e94434236955c9ea8
uki <ukiworks034@gmail.com>
2022-03-09 16:12:28 -0500

    7/11 probs completed

--- problems/07-all-the-args.js
+++ problems/07-all-the-args.js
@@ -1,6 +1,18 @@
 function allTheArgs(func, ...args) {
-  // Your code here
+  return func.bind(func, ...args);
 }
+
+function sum(...nums) {
+  return nums.reduce((acc, num) => acc + num);
+}
+
+const onePlusTwo = allTheArgs(sum, 1, 2);
+
+const onePlusTwoPlusThree = onePlusTwo(3);
+const onePlusTwoPlusFour = onePlusTwo(4);
+
+console.log(onePlusTwoPlusThree); // => 6
+console.log(onePlusTwoPlusFour);  // => 7
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
--- problems/08-call-me-later.js
+++ problems/08-call-me-later.js
@@ -1,4 +1,14 @@
-// Your code here
+class CallCenter {
+	constructor(name,) {
+		this.name = name;
+	}
+	sayHello() {
+		console.log(`Hello this is ${this.name}`);
+	}
+	callMeLater(delay) {
+		setTimeout(() => this.sayHello())
+	}
+}
 
 
 /*****************************************************************************/
